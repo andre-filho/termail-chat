3698e2b90b2db52f20ecf647ab2dd7738d0552db
matheusbsilva <matheusbattista@hotmail.com>
2019-05-21 11:33:54 -0300

    change mq_open to create fifo if not exists
        - Still with error of invalid args
    
    Co-authored-by: andre <andre.filho001@outlook.com>

--- main.c
+++ main.c
@@ -17,6 +17,10 @@
 #include <fcntl.h> 
 #include <sys/stat.h>
 #include <mqueue.h>
+#include <errno.h>
+#include <string.h>
+#define QUEUE_PERMISSIONS 0660
+extern int errno;
 
 void do_the_fork_stuff(pid_t f_pid)
 {
@@ -37,13 +41,37 @@ int main(int argc, char const *argv[])
 
   mqd_t q1;
 
-  q1 = mq_open("teste", O_RDWR);
+  struct mq_attr attr;
+
+  attr.mq_flags = 0;
+  attr.mq_maxmsg = 30;
+  attr.mq_msgsize = 256; 
+  attr.mq_curmsgs = 0;
+
+  q1 = mq_open("/porra", O_RDONLY | O_CREAT, QUEUE_PERMISSIONS, &attr);
+
+  if(q1 == -1) {
+      int errnum;
+      char *errmsg;
+
+      errnum = errno;
+      errmsg = strerror(errnum);
+      fprintf(stderr, "Error: %s \n", errmsg);
+      return -1;
+  }
 
   char *msg = "asdf";
   char *msg2;
   msg2 = (char *) malloc(5 * sizeof(char));
 
-  mq_send(q1, msg, sizeof(msg), 1);
+  int send_return;
+
+  send_return = mq_send(q1, msg, sizeof(msg), 1);
+
+  if(send_return != 0) {
+      printf("Error on send message to the FIFO %d\n", send_return);
+      return -1;
+  }
 
   mq_receive(q1, (void*) msg, sizeof(msg), 1);
 
