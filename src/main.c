9707cf1bd83a55ec622ae2407d758d88321e36f0
matheusbsilva <matheusbattista@hotmail.com>
2019-06-02 16:08:32 -0300

    create function to register username

--- src/main.c
+++ src/main.c
@@ -1,15 +1,15 @@
 /*
-  dois processo que manda os sinal entre eles
-  tratar o ctrl+c (SIGINT que fecha a parada)
-  10 char para cd nome e 500 de mensagem
+   dois processo que manda os sinal entre eles
+   tratar o ctrl+c (SIGINT que fecha a parada)
+   10 char para cd nome e 500 de mensagem
 
-  user    ~> rw
-  group   ~> w
-  others  ~> w
+   user    ~> rw
+   group   ~> w
+   others  ~> w
 
-  fechar: /dev/mqueue e deleta saporra
+fechar: /dev/mqueue e deleta saporra
 
-  http://wiki.inf.ufpr.br/maziero/lib/exe/fetch.php?media=socm:socm-texto-09.pdf
+http://wiki.inf.ufpr.br/maziero/lib/exe/fetch.php?media=socm:socm-texto-09.pdf
 */
 
 #include "aux.h"
@@ -19,55 +19,70 @@
 
 // mq_maxmsg não pode ser maior que o valor definido em
 // /proc/sys/fs/mqueue/queues_max
+//
+
+void register_user(char *username) 
+{
+    printf("Bem vindo ao Chat!\n");
+    printf("Qual seu nome de usuário?(Máximo 10 caracteres)\n");
+    scanf("%s", username);
+}
 
 
 int main(int argc, char const *argv[])
 {
 
-  mqd_t q1;
+    char *username;
+    username = (char *)malloc(MAX_USERNAME_SIZE * sizeof(char));
 
-  pthread_t t_receive;
-  pthread_t t_send;
+    register_user(username);
 
-  char *msg = "olá tudo bem";
-  char msg2[8200];
+    printf("Olá %s\n", username);
 
-  struct mq_attr attr;
+    mqd_t q1;
 
-  config_mq(&attr);
+    pthread_t t_receive;
+    pthread_t t_send;
 
-  debug_log("Creating FIFO");
-  q1 = mq_open(FIFO_NAME, O_RDWR | O_CREAT, 0666, &attr);
+    char *msg = "olá tudo bem";
+    char msg2[8200];
 
-  if (q1 == -1)
-  {
-    perror("mq_open");
-    return -1;
-  }
+    struct mq_attr attr;
 
-  if (mq_getattr(q1, &attr) == -1)
-  {
-    perror("mq_getattr");
-    return -1;
-  }
+    config_mq(&attr);
 
-  printf("Maximum # of messages on queue: %ld\n", attr.mq_maxmsg);
-  printf("Maximum message size: %ld\n", attr.mq_msgsize);
+    debug_log("Creating FIFO");
+    q1 = mq_open(FIFO_NAME, O_RDWR | O_CREAT, 0666, &attr);
 
-  struct msg_args read_params;
+    if (q1 == -1)
+    {
+        perror("mq_open");
+        return -1;
+    }
 
-  read_params.q = q1;
-  read_params.msg = msg2;
+    if (mq_getattr(q1, &attr) == -1)
+    {
+        perror("mq_getattr");
+        return -1;
+    }
 
-  debug_log("Starting thread to send message");
-  pthread_create(&t_send, NULL, &send_message, msg);
+    printf("Maximum # of messages on queue: %ld\n", attr.mq_maxmsg);
+    printf("Maximum message size: %ld\n", attr.mq_msgsize);
 
-  debug_log("Starting thread to receive messages");
-  pthread_create(&t_receive, NULL, &verify_new_message, &read_params);
+    struct msg_args read_params;
 
-  printf("\n\n");
+    read_params.q = q1;
+    read_params.msg = msg2;
 
-  /* mq_unlink(FIFO_NAME); */
+    debug_log("Starting thread to send message");
+    pthread_create(&t_send, NULL, &send_message, msg);
 
-  return 0;
+    debug_log("Starting thread to receive messages");
+    pthread_create(&t_receive, NULL, &verify_new_message, &read_params);
+
+    printf("\n\n");
+
+    /* mq_unlink(FIFO_NAME); */
+
+    return 0;
 }
