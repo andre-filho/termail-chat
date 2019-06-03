0e11789afe960ddbba2f7ddacb9b5e10ca2bf4db
Andre Filho <andre.filho001@outlook.com>
2019-06-03 15:57:33 -0300

    Commented and deleted some unused macros

--- inc/vars.h
+++ inc/vars.h
@@ -1,14 +1,13 @@
 #if !defined(VARS_H)
 #define VARS_H
 
-#define CREATOR_PERMISSON 0662
-#define CONSUMER_PERMISSON 0222
-#define W_PERMISSON 0
+#define MAX_MSG 10
+// #define W_PERMISSON 0
+#define MAX_MSG_SIZE 500
 #define QUEUE_PREFIX "/chat-"
 #define MAX_USERNAME_SIZE 10
-#define MAX_MSG_SIZE 500
-#define MAX_MSG 10
-#define FIFO_NAME "/fifo"
+#define CREATOR_PERMISSON 0662
+// #define CONSUMER_PERMISSON 0222
 #define FULL_MSG_SIZE MAX_MSG_SIZE + MAX_USERNAME_SIZE * 2 + 2
 
 #endif // VARS_H
