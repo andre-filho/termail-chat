9c39a05c4026e7c82a9e18e34671a2b18f78077e
matheusbsilva <matheusbattista@hotmail.com>
2019-06-04 00:13:39 -0300

    change permission of fifo file
    Co-authored-by: andre <andre.filho001@outlook.com>

--- inc/vars.h
+++ inc/vars.h
@@ -6,7 +6,7 @@
 #define MQ_DIR "/dev/mqueue/"
 #define QUEUE_PREFIX "/chat-"
 #define MAX_USERNAME_SIZE 10
-#define CREATOR_PERMISSON 0662
+#define CREATOR_PERMISSON 0622
 #define FULL_MSG_SIZE MAX_MSG_SIZE + MAX_USERNAME_SIZE * 2 + 2
 
 #endif // VARS_H
